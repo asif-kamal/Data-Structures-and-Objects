0e4f99b27f65b74c3b7e7a7800ffaf4e4a1f46d0
asif-kamal <asif.kamal1489@gmail.com>
2023-05-25 19:20:24 -0400

    Built updateTally program

--- LookingOutForNumOne.java
+++ LookingOutForNumOne.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LookingOutForNumOne {
     public static int countDigits(int num) {
 
@@ -30,24 +32,38 @@ public class LookingOutForNumOne {
     }
 
     public static String nthDigit(int n, int num) {
-        
+
         String digits = Integer.toString(num);
 
         if (n >= digits.length()) {
-            System.out.println(0);
+            // System.out.println(0);
             return Integer.toString(0);
         } else {
             char[] arr = digits.toCharArray();
-            System.out.println(arr[n]);
+            // System.out.println(arr[n]);
             String result = String.valueOf(arr[n]);
             return result;
         }
     }
 
+    public static void updateTally(int n, int num, int[] tally) {
+
+        // {0,0,1,2,0,0,3,0,9,0}
+        int index = Integer.parseInt(nthDigit(n, num));
+        tally[index] = tally[index] + 1;
+
+        for (int element : tally) {
+            System.out.println(element);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         // countDigits(3000);
-        //nthDigitBack(1, 1998);
-        nthDigit(5, 19998);
+        // nthDigitBack(1, 1998);
+        // nthDigit(5, 19998);
+        int[] tally = { 0, 0, 1, 2, 0, 0, 3, 0, 9, 0 };
+        updateTally(4, 1998361, tally);
     }
 }
