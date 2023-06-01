149493fb1b5d776e38a44cee0f71db4ac199c017
asif-kamal <asif.kamal1489@gmail.com>
2023-05-31 20:06:34 -0400

    Built detectIfWinner() method

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,12 +4,13 @@ public class TicTacToe {
 
     public TicTacToe(char[][] board) {
         this.board = board;
-        this.board = new char[][] { {'-','-','-'}, {'-','-','-'}, {'-','-','-'} }; //single quotes for char array
-    
+        this.board = new char[][] { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } }; // single quotes for char
+                                                                                               // array
+
     }
 
     public void move(int row, int col) {
-        if(isXTurn) {
+        if (isXTurn) {
             this.board[row][col] = 'X';
             isXTurn = false;
         } else {
@@ -26,25 +27,93 @@ public class TicTacToe {
             System.out.println();
         }
         // System.out.println(Arrays.deepToString(this.board));
-    } 
+    }
 
     public void isWhoseTurn() {
-        if(isXTurn) {
+        if (isXTurn) {
             System.out.println("X");
         } else {
             System.out.println("O");
         }
     }
 
-    public void detectIfWinner() {
-        
+    public char detectIfWinner() {
+        if ((this.board[0][0] == 'O') && (this.board[0][1] == 'O') && (this.board[0][2] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[1][0] == 'O') && (this.board[1][1] == 'O') && (this.board[1][2] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][0] == 'O') && (this.board[2][1] == 'O') && (this.board[2][2] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][0] == 'O') && (this.board[1][0] == 'O') && (this.board[2][0] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][1] == 'O') && (this.board[1][1] == 'O') && (this.board[2][1] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][2] == 'O') && (this.board[1][2] == 'O') && (this.board[2][2] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][0] == 'O') && (this.board[1][1] == 'O') && (this.board[0][2] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][2] == 'O') && (this.board[1][1] == 'O') && (this.board[0][0] == 'O')) {
+            char win = 'O';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][0] == 'X') && (this.board[0][1] == 'X') && (this.board[0][2] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[1][0] == 'X') && (this.board[1][1] == 'X') && (this.board[1][2] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][0] == 'X') && (this.board[2][1] == 'X') && (this.board[2][2] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][0] == 'X') && (this.board[1][0] == 'X') && (this.board[2][0] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][1] == 'X') && (this.board[1][1] == 'X') && (this.board[2][1] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[0][2] == 'X') && (this.board[1][2] == 'X') && (this.board[2][2] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][0] == 'X') && (this.board[1][1] == 'X') && (this.board[0][2] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else if ((this.board[2][2] == 'X') && (this.board[1][1] == 'X') && (this.board[0][0] == 'X')) {
+            char win = 'X';
+            System.out.println("There's a winner!");
+            return win;
+        } else {
+            return '-';
+        }
     }
 
     public void winner() {
-
+        if (detectIfWinner() != '-') {
+            System.out.println(detectIfWinner());
+        }
     }
 
-
-
-
+    public void restartGame() {
+        new TicTacToe(new char[0][0]);
+    }
 }
