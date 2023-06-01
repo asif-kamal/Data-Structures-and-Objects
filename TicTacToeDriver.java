7ca8f53334daab067a5ceb762c9357186ed59834
asif-kamal <asif.kamal1489@gmail.com>
2023-05-31 21:09:05 -0400

    Called methods on TicTacToe instance

--- TicTacToeDriver.java
+++ TicTacToeDriver.java
@@ -2,9 +2,26 @@ public class TicTacToeDriver {
 
     public static void main(String[] args) {
 
-        TicTacToe t1 = new TicTacToe(new char[0][0]);
+        TicTacToe t1 = new TicTacToe(new char[0][0]); //use 'new' keyword inside parameters to initialize new char array
 
+        // t1.displayTheBoard();
+        // t1.isWhoseTurn();
+        t1.move(0, 0);//(2,2) does not work
         t1.displayTheBoard();
+        t1.isWhoseTurn();
+        t1.move(0,2);
+        t1.displayTheBoard();
+        t1.move(1, 0);
+        t1.displayTheBoard();
+        t1.move(1,2);
+        t1.displayTheBoard();
+        t1.move(2,0);
+        t1.displayTheBoard();
+        t1.detectIfWinner();
+        t1.winner();
+        t1.restartGame();
+        t1.displayTheBoard();
+        
     }
     
 }
