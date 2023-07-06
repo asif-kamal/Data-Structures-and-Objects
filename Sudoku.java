83d8c63909b5f7f6102869c6bd28a0ca85075e87
asif-kamal <asif.kamal1489@gmail.com>
2023-07-05 22:30:03 -0400

    Sudoku solver built, has no errors

--- Sudoku.java
+++ Sudoku.java
@@ -1,17 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Sudoku {
 
+    private static int sum = 0;
+
     public static boolean solveSudoku(int[][] board, int row, int col) {
+
         if (col == board.length) {
             row++;
             col = 0;
         }
         if (row == board.length) {
+            for (int[] rowN : board) {
+                System.out.println(Arrays.toString(rowN));
+            }
+            System.out.println();
+            sum += (100*board[0][0] + 10*board[0][1] + board[0][2]);
             return true;
         }
         if (board[row][col] != 0) {
-            solveSudoku(board, row, col + 1);
+            return solveSudoku(board, row, col + 1);
         }
         for (int i = 1; i <= board.length; i++) {
             if (isValid(board, row, col, i)) {
@@ -19,6 +30,7 @@ public class Sudoku {
                 if (solveSudoku(board, row, col + 1)) {
                     return true;
                 }
+                board[row][col] = 0;
             }
         }
         return false;
@@ -52,25 +64,14 @@ public class Sudoku {
 
     private static boolean isBoxValid(int[][] board, int row, int col, int i) {
         // how to check the 3x3 box for duplicates?
-        if (col != 1 || col != 2 || col != 4 || col != 5 || col != 7 || col != 8) {
-            for (int j = 0; j < 3; j++) {
-                // each 3x3 box starts from 0-2, 3-5, and 6-8
-                // figure out how to check a 3x3 box
-                if (board[row][j] == i || board[row + 1][j] == i || board[row + 2][j] == i) {
+        int subGridRow = (row / 3) * 3;
+        int subGridCol = (col / 3) * 3;
+
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (board[subGridRow + j][subGridCol + k] == i) {
                     return false;
                 }
-            }
-        } else if (col == 1 || col == 4 || col == 7) {
-            // each 3x3 box starts from 0-2, 3-5, and 6-8
-            // figure out how to check a 3x3 box
-            if (board[row][col + 1] == i || board[row + 1][col + 1] == i || board[row + 2][col + 1] == i
-                    || board[row][col - 1] == i || board[row + 1][col - 1] == i || board[row + 2][col - 1] == i) {
-                return false;
-            }
-        } else if (col == 2 || col == 5 || col == 8) {
-            if (board[row][col - 1] == i || board[row + 1][col - 1] == i || board[row + 2][col - 1] == i
-                    || board[row][col - 2] == i || board[row + 1][col - 2] == i || board[row + 2][col - 2] == i) {
-                return false;
             }
         }
         return true;
@@ -78,22 +79,28 @@ public class Sudoku {
 
     public static void main(String[] args) {
         // int[][] board = new int[9][9];
-        int[][] board = {
-                { 0, 0, 3, 0, 2, 0, 6, 0, 0 },
-                { 9, 0, 0, 3, 0, 5, 0, 0, 1 },
-                { 0, 0, 1, 8, 0, 6, 4, 0, 0 },
-                { 0, 0, 8, 1, 0, 2, 9, 0, 0 },
-                { 7, 0, 0, 0, 0, 0, 0, 0, 8 },
-                { 0, 0, 6, 7, 0, 8, 2, 0, 0 },
-                { 0, 0, 2, 6, 0, 9, 5, 0, 0 },
-                { 8, 0, 0, 2, 0, 3, 0, 0, 9 },
-                { 0, 0, 5, 0, 1, 0, 3, 0, 0 }
-        };
-        solveSudoku(board, 0, 0);
-        for (int[] rowN : board) {
-            System.out.println(Arrays.toString(rowN));
-        }
 
+        try {
+            Scanner s = new Scanner(new File("p096_sudoku.txt"));
+
+            for (int i = 0; i < 50; i++) {
+                s.nextLine();
+                int[][] result = new int[9][9];
+                for (int j = 0; j < 9; j++) {
+                    String n = s.nextLine();
+                    char[] nn = n.toCharArray();
+                    for (int k = 0; k < 9; k++) {
+                        result[j][k] = nn[k] - '0';
+                    }
+                }
+                solveSudoku(result, 0, 0);
+            }
+            System.out.println(Sudoku.sum);
+
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
     }
+
 
 }
