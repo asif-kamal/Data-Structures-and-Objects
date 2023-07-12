50a647f157b4dacbd36002a157b6b1013435c065
asif-kamal <asif.kamal1489@gmail.com>
2023-07-12 12:36:58 -0400

    Added counter for line number

--- Module 11 files/ReadingFromFileExample.java
+++ Module 11 files/ReadingFromFileExample.java
@@ -1,34 +1,35 @@
 // package index;
 
-
 import java.io.*;
 import java.util.Scanner;
 
 public class ReadingFromFileExample {
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 
 		String fileName = "pg100.txt";
-		
+		int lineNumberCount = 0;
+
 		try {
 			Scanner scanner = new Scanner(new File(fileName));
-			while(scanner.hasNextLine()){
+			while (scanner.hasNextLine()) {
+				// if (scanner.nextLine() != "") {
+				lineNumberCount++;
+				// }
 				String line = scanner.nextLine();
-				System.out.println(line);
+				System.out.println(line + " - Line Number: " + lineNumberCount);
 				String[] words = line.split("\\s+");
-				for(String word : words){
-				word = word.replaceAll("[\\p{Punct}\\s]+", "");
-				System.out.println(word);
+				for (String word : words) {
+					word = word.replaceAll("[\\p{Punct}\\s]+", "");
+					System.out.println(word);
 				}
 			}
 			scanner.close();
-			
+
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 	}
 }
-
-
