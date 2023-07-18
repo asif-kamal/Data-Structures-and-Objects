e117be7385fab459e58788ecfb73fdb10e7e6e70
asif-kamal <asif.kamal1489@gmail.com>
2023-07-17 20:02:04 -0400

    Added FileWriter to csv file

---
+++ InsertionSort.java
@@ -0,0 +1,91 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+//Insertion Method from: https://chat.openai.com
+
+public class InsertionSort {
+
+    private static class Data {
+        long exchanges;
+        long comparisons;
+        long elapsedTime;
+
+        public String toString() {
+            return comparisons + " " + exchanges + " " + elapsedTime;
+        }
+    }
+
+    public static int[] makeArray(int length) {
+        Random rand = new Random();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(10001);
+        }
+        return arr;
+    }
+
+    public static void insertionSort(int[] arr, Data data) {
+        int n = arr.length;
+        long start = System.nanoTime();
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            data.comparisons++;// increment before entering while loop
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+                data.exchanges++;
+                data.comparisons++; // increment while in while loop
+            }
+            arr[j + 1] = key;
+            data.exchanges++;
+        }
+        long end = System.nanoTime();
+        data.elapsedTime = end - start;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Data> dataList = new ArrayList<>();
+        ArrayList<String> outputArr = new ArrayList<>();
+        long sumOfExchanges = 0;
+        long sumOfComparisons = 0;
+        long sumOfElapsedTime = 0;
+
+        for (int i = 6; i <= 16; i++) {
+            int n = (int) Math.pow(2, i);
+            for (int j = 0; j < 100; j++) {
+                int[] arr = makeArray(n);
+                Data d = new Data();
+                insertionSort(arr, d);
+                dataList.add(d);
+                sumOfExchanges += d.exchanges;
+                sumOfComparisons += d.comparisons;
+                sumOfElapsedTime += d.elapsedTime;
+            }
+            System.out
+                    .println("n (array length): " + n + "\t" + "Average Out of 100" + "\t" + "Exchanges: "
+                            + sumOfExchanges / 100 + "\t"
+                            + "Comparisons: "
+                            + sumOfComparisons / 100
+                            + "\t" + "Elapsed Time(ns): " + sumOfElapsedTime / 100);
+
+            String output = "n (array length): " + n + "\t" + "Average Out of 100" + "\t" + "Exchanges: "
+                    + sumOfExchanges / 100 + "\t"
+                    + "Comparisons: "
+                    + sumOfComparisons / 100
+                    + "\t" + "Elapsed Time(ns): " + sumOfElapsedTime / 100;
+
+            outputArr.add(output);
+        }
+        // Write the data to a CSV file
+        FileWriter fw = new FileWriter("results.csv");
+        for (String line : outputArr) {
+            fw.write(line + "\n");
+        }
+        fw.close();
+    }
+}
